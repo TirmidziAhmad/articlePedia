cf8864cfa0a702df47db45b48866ee175bf09270
Tirmidzi Ahmad <tirmidziahmad05@gmail.com>
2025-06-30 16:29:43 +0700

    add: redirect login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,5 @@
-"use client";
-import { useRouter } from "next/navigation";
-import toast from "react-hot-toast";
+import { redirect } from "next/navigation";
 
-export default function UserHeader() {
-  const router = useRouter();
-
-  const handleLogout = () => {
-    localStorage.removeItem("userId");
-    localStorage.removeItem("userRole");
-    localStorage.removeItem("isLoggedIn");
-    // Jika Anda menyimpan di cookies, hapus juga cookiesnya
-    // document.cookie = 'isLoggedIn=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-    // document.cookie = 'userRole=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-
-    toast.success("Anda telah logout.");
-    router.push("/login"); // Redirect ke halaman login
-  };
-
-  return (
-    <nav>
-      {/* ... navigasi lainnya */}
-      <button onClick={handleLogout}>Logout</button>
-    </nav>
-  );
+export default function Home() {
+  redirect("/login");
 }
